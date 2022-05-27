41f01713d43d2783c6a088d942b6fafc95300aa2
kecoz-dev <55542635+kecoz-dev@users.noreply.github.com>
2022-05-27 17:35:49 +0900

    dd

--- src/main/java/com/kecoz/Toy1/conteroller/member/MemberController.java
+++ src/main/java/com/kecoz/Toy1/conteroller/member/MemberController.java
@@ -1,4 +1,16 @@
 package com.kecoz.Toy1.conteroller.member;
 
 public class MemberController {
+
+    String id;
+    String password;
+    String loginDate;
+    int loginCount;
+
+
+
+    public MemberController(){
+
+    }
+
 }
